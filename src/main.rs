8a59cda867b86826bc08400c8530688cbe02c4da
malken21 <77374813+malken21@users.noreply.github.com>
2025-08-10 20:17:37 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,12 @@
-use serde::Deserialize;
 use std::time::Duration;
 
-// --- 構造体定義 --- start
-#[derive(Deserialize, Debug)]
-#[allow(dead_code)]
-struct AircraftData {
-    now: f64,
-    messages: i64,
-    aircraft: Vec<Aircraft>,
-}
+mod aircraft;
+use aircraft::aircraft;
 
-#[derive(Deserialize, Debug)]
-#[allow(dead_code)]
-struct Aircraft {
-    hex: String,
-    flight: Option<String>,
-    squawk: Option<String>,
-    alt_baro: Option<serde_json::Value>,
-    lat: Option<f64>,
-    lon: Option<f64>,
-    gs: Option<f64>,
-    track: Option<f64>,
-    category: Option<String>,
-    messages: i64,
-    rssi: f64,
-}
-// --- 構造体定義 --- end
+let url = env::var("TAR1090_URL").unwrap_or_else(|_| "http://localhost:8080/data/aircraft.json".to_string());
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let url = "http://192.168.0.55:8080/data/aircraft.json";
     let client = reqwest::Client::new();
     let mut interval = tokio::time::interval(Duration::from_secs(3));
     loop {
@@ -40,25 +17,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 if res.status().is_success() {
                     match res.text().await {
                         Ok(body_text) => {
-                            // テキストから新しいstructへのパースを試みる
-                            match serde_json::from_str::<AircraftData>(&body_text) {
-                                Ok(data) => {
-                                    // 検出したすべての航空機情報を簡単に出力する
-                                    for aircraft in data.aircraft {
-                                        println!(
-                                            "機体: {}, 便名: {:?}, 高度: {:?}, 緯度経度: ({:?}, {:?})",
-                                            aircraft.hex,
-                                            aircraft.flight.map(|s| s.trim().to_string()), // 前後の空白を削除
-                                            aircraft.alt_baro,
-                                            aircraft.lat,
-                                            aircraft.lon
-                                        );
-                                    }
-                                }
-                                Err(e) => {
-                                    eprintln!("JSON parsing failed: {}", e);
-                                }
-                            }
+                            aircraft(&body_text);
                         }
                         Err(e) => eprintln!("Failed to read response body: {}", e),
                     }
