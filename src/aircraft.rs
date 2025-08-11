96934b5599410c3ac523b25fc0c561798d8f9f13
malken21 <77374813+malken21@users.noreply.github.com>
2025-08-12 03:48:21 +0900

    Update aircraft.rs

--- src/aircraft.rs
+++ src/aircraft.rs
@@ -36,11 +36,16 @@ pub struct Watcher {
 }
 
 impl Watcher {
-    pub fn new(value: Vec<&str>, discord_webhook_url: &str, cooldown: &f64) -> Self {
+    pub fn new(
+        value: Vec<&str>,
+        discord_webhook_url: &str,
+        cooldown: &f64,
+        payload_template: &str
+    ) -> Self {
         println!("Check Flights: {:?}", value);
         Self {
             check_flights: value.into_iter().map(String::from).collect(),
-            discord: DiscordWebhook::new(discord_webhook_url, cooldown),
+            discord: DiscordWebhook::new(discord_webhook_url, cooldown, payload_template),
         }
     }
 
